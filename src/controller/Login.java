33f78b7a76f2609846231a4c449aceb4605dd988
Jasmine Langford <jkhatt0788@gmail.com>
2022-12-25 17:22:34 -0500

    Updated toMainMenu method to loginButton.

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,14 +1,21 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
+import main.Main;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -21,17 +28,45 @@ import java.util.ResourceBundle;
  * */
 public class Login implements Initializable {
 
+    public PasswordField username;
+    public PasswordField passwordLogin;
+
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Label languageLabel;
+    @FXML
+    private Label timeZoneLabel;
+    @FXML
+    private Label logoTagline;
+    @FXML
+    private Label signInLabel;
+    @FXML
+    private Button loginButtonLabel;
+
     @Override
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("I am initialized.");
+
+        ResourceBundle rb = ResourceBundle.getBundle("Nat", Locale.getDefault());
+        usernameLabel.setText(rb.getString("usernameLabel"));
+        passwordLabel.setText(rb.getString("passwordLabel"));
+        languageLabel.setText(rb.getString("language"));
+        timeZoneLabel.setText(rb.getString("timezone"));
+        logoTagline.setText(rb.getString("tagline"));
+        signInLabel.setText(rb.getString("signInLabel"));
+        loginButtonLabel.setText(rb.getString("loginButtonLabel"));
     }
+
 
     /**
      * This navigates the user to the Main Menu upon successful login.
      * @param actionEvent Login button is clicked.
      * */
-    public void toMainMenu(ActionEvent actionEvent) throws IOException {
+    public void loginButton(ActionEvent actionEvent) throws IOException {
 
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/main-menu.fxml"))));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
