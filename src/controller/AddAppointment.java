2bf2d93c9c99435fa2a7573b71265676be22e72c
Jasmine Langford <jkhatt0788@gmail.com>
2023-01-23 23:52:44 -0500

    Added end-user form fields for input validation messages

--- src/controller/AddAppointment.java
+++ src/controller/AddAppointment.java
@@ -1,23 +1,31 @@
 package controller;
 
+import DAO.AppointmentDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.Appointment;
+
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 /**
  * This class is the controller for the add-appointment.fxml view.
- * User will be able to input data into text fields and choice boxes in order to add a new appointment.
+ *
+ * End-user will be able to input data into text fields and choice boxes in order to add a new appointment.
  */
-public class AddAppointment {
+public class AddAppointment implements Initializable {
 
     // Form fields
     @FXML
@@ -45,7 +53,17 @@ public class AddAppointment {
     @FXML
     private TextField userIdTextfield;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Add appointment is initialized!");
 
+        // generates unique appointment id for new appointment
+        try {
+            apptID.setText(String.valueOf(AppointmentDAO.allAppointments().size()+1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * This will add the data from form fields as a new appointment to database.
@@ -54,12 +72,27 @@ public class AddAppointment {
      */
     public void onSaveAppt(ActionEvent actionEvent) {
 
-        // Exception dialog messages
+        // end-user form fields
+        String addStartDate = startDatePicker.getValue().format(DateTimeFormatter.ofPattern("dd MMMM yyyy zzzz"));
+        String addStartTime = (String) startTimeCombo.getValue().toString();
+        String addEndDate = endDatePicker.getValue().format(DateTimeFormatter.ofPattern("dd MMMM yyyy zzzz"));
+        String addEndTime = (String) endTimeCombo.getValue().toString();
+        //int apptId = Integer.parseInt(apptID.getText());
+        String addContact = contactCombo.getValue().toString();
+        String addType = typeTextfield.getText();
+        String addTitle = titleTextfield.getText();
+        String addDescription = descTextfield.getText();
+        String addLocation = locationTextfield.getText();
+        int addCustID = Integer.parseInt(custIdTextfield.getText());
+        int addUserID = Integer.parseInt(userIdTextfield.getText());
+
+        // Input Validation Messages
         try {
-            if (typeTextfield.getText().isEmpty() || titleTextfield.getText().isEmpty() ||
-                    descTextfield.getText().isEmpty() || locationTextfield.getText().isEmpty()) {
-                Alert emptyField = new Alert(Alert.AlertType.ERROR, "Invalid data type. Please enter value in each "
-                                   + "form field.");
+            if (addStartDate.isEmpty() || addEndDate.isEmpty() ||addStartTime.isEmpty() || addEndTime.isEmpty() ||
+                    addContact.isEmpty() || addType.isEmpty() || addTitle.isEmpty() || addDescription.isEmpty() ||
+                    addLocation.isEmpty()) {
+                Alert emptyField = new Alert(Alert.AlertType.ERROR, "One or more fields are empty. Please enter a" +
+                        " value in each field.");
                 Optional<ButtonType> results = emptyField.showAndWait();
                 if (results.isPresent() && results.get() == ButtonType.OK)
                     return;
@@ -69,7 +102,7 @@ public class AddAppointment {
         }
 
         try {
-            Integer.parseInt(custIdTextfield.getText());
+            Integer.parseInt(String.valueOf(addCustID));
         } catch (NumberFormatException e) {
             Alert invalidDataType = new Alert(Alert.AlertType.ERROR, "Customer ID should be an integer.");
             Optional<ButtonType> results = invalidDataType.showAndWait();
@@ -78,7 +111,7 @@ public class AddAppointment {
         }
 
         try {
-            Integer.parseInt(userIdTextfield.getText());
+            Integer.parseInt(String.valueOf(addUserID));
         } catch (NumberFormatException e) {
             Alert invalidDataType = new Alert(Alert.AlertType.ERROR, "User ID should be an integer.");
             Optional<ButtonType> results = invalidDataType.showAndWait();
@@ -86,18 +119,13 @@ public class AddAppointment {
                 return;
         }
 
-         String addStartDate = startDatePicker.getValue().format(DateTimeFormatter.ofPattern("dd MMMM yyyy zzzz"));
-         int addStartTime = startTimeCombo.getSelectionModel().getSelectedIndex();
-         String addEndDate = endDatePicker.getValue().format(DateTimeFormatter.ofPattern("dd MMMM yyyy zzzz"));
-         int addEndTime = endTimeCombo.getSelectionModel().getSelectedIndex();
-         int id = Integer.parseInt(apptID.getText());
-         String addContact = contactCombo.getValue().toString();
-         String addType = typeTextfield.getText();
-         String addTitle = titleTextfield.getText();
-         String addDescription = descTextfield.getText();
-         String addLocation = locationTextfield.getText();
-         int addCustID = Integer.parseInt(custIdTextfield.getText());
-         int addUserID = Integer.parseInt(userIdTextfield.getText());
+         Alert addAppointmentConfirm = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to add this " +
+                 "appointment?");
+         Optional<ButtonType> result = addAppointmentConfirm.showAndWait();
+         if(result.isPresent() && result.get() == ButtonType.YES) {
+
+
+         }
 
     }
 
@@ -114,4 +142,6 @@ public class AddAppointment {
         stage.centerOnScreen();
         stage.setResizable(false);
     }
+
+
 }
