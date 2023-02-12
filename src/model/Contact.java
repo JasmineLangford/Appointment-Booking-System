7fe46dd02c954f341afec84c4a50c5117b644092
Jasmine Langford <jkhatt0788@gmail.com>
2023-02-11 22:28:29 -0500

    Set up contact model for query

---
+++ src/model/Contact.java
@@ -0,0 +1,48 @@
+package model;
+
+/**
+ * This class is the model for contacts.
+ */
+
+public class Contact {
+
+    private int contactId;
+    private String contactName;
+
+    /**
+     * Constructor
+     */
+
+    public Contact (int contactId, String contactName){
+        this.contactId = contactId;
+        this. contactName = contactName;
+    }
+
+    /**
+     * @return contact ID
+     */
+    public int getContactId() {
+        return contactId;
+    }
+
+    /**
+     * @param  contactId to set
+     */
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    /**
+     * @return contact name
+     */
+    public String getContactName() {
+        return contactName;
+    }
+
+    /**
+     * @param contactName to set
+     */
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+}
