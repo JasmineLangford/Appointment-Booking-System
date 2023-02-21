08a1d8512860cbe6e89e725dd38fb3d3e8e13f95
Jasmine Langford <jkhatt0788@gmail.com>
2023-02-21 11:58:12 -0500

    Final Javadoc edits

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,7 +1,7 @@
 package model;
 
 /**
- * This class is the model class for customer.
+ * This class represents a customer.
  */
 public class Customer {
 
@@ -17,9 +17,20 @@ public class Customer {
     private int totalCustomers;
 
     /**
-     * Constructor
+     * This is the constructor representing all customer information.
+     *
+     * @param customerId Auto-incremented ID from the database.
+     * @param customerName The customer's first and last name.
+     * @param customerAddress The customer's address.
+     * @param customerPhone The customer's phone number.
+     * @param customerCountry The customer's country name.
+     * @param countryId The int related to the customer country.
+     * @param divisionId The int related to the customer state or province.
+     * @param division The customer's state or province name.
+     * @param customerPostal The customer's postal code.
      */
-    public Customer(int customerId, String customerName, String customerAddress, String customerPhone, String customerCountry, int countryId, int divisionId, String division, String customerPostal){
+    public Customer(int customerId, String customerName, String customerAddress, String customerPhone, String
+            customerCountry, int countryId, int divisionId, String division, String customerPostal){
         this.customerId = customerId;
         this.customerName = customerName;
         this.customerAddress = customerAddress;
@@ -32,11 +43,13 @@ public class Customer {
     }
 
     /**
-     * Constructor for query of first-level divisions for combo box selection
+     * This is the constructor representing customer's state or province and the total customers calculated.
+     * This is used for reporting.
      *
-     * @param divisionId to get
-     * @param division to get
-     * @param countryId to get
+     * @param divisionId The int related to the customer's state or province.
+     * @param division The customer's state or province name.
+     * @param countryId The int related to the customer country.
+     * @param totalCustomers The sum of customers in the same state or province.
      */
     public Customer(int divisionId, String division, int countryId, int totalCustomers) {
         this.divisionId = divisionId;
@@ -46,150 +59,213 @@ public class Customer {
     }
 
     /**
-     * Constructor for query of country selections for combo box.
+     * This is the constructor representing the customer's country.
+     * Used for country combo box selection.
      *
-     * @param countryId to get
-     * @param customerCountry to get
+     * @param countryId The int related to the customer country.
+     * @param customerCountry The customer's country name.
      */
     public Customer(int countryId, String customerCountry) {
         this.countryId = countryId;
         this.customerCountry = customerCountry;
     }
 
-    // null
+    /**
+     * This is the constructor representing customer's state or province.
+     * This is used for the state/province combo box selection.
+     *
+     * @param divisionId The int related to the customer's state or province.
+     * @param division The customer's state or province name.
+     * @param countryId The int related to the customer country.
+     */
+    public Customer(int divisionId, String division, int countryId) {
+        this.divisionId = divisionId;
+        this.division = division;
+        this.countryId = countryId;
+    }
+
+    /**
+     * Null method for customer.
+     */
     public Customer() {
     }
 
-
     /**
-     * @return the custID
+     * Gets the customer ID.
+     *
+     * @return An integer representing the customer ID.
      */
     public int getCustomerId() {
         return customerId;
     }
 
     /**
-     * @param customerId to set
+     * Sets the customer ID.
+     *
+     * @param customerId An integer containing the customer ID.
      */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
     /**
-     * @return the customerName
+     * Gets the customer name.
+     *
+     * @return A string representing the customer name.
      */
     public String getCustomerName() {
         return customerName;
     }
 
     /**
-     * @param customerName to set
+     * Sets the customer name.
+     *
+     * @param customerName A string containing the customer name.
      */
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
     /**
-     * @return the customer address
+     * Gets the customer address.
+     *
+     * @return A string representing the customer address.
      */
     public String getCustomerAddress() {
         return customerAddress;
     }
 
     /**
+     * Sets the customer address.
+     *
+     * @param customerAddress A string containing the customer address.
      */
     public void setCustomerAddress(String customerAddress) {
         this.customerAddress = customerAddress;
     }
 
     /**
-     * @return the customer phone number
+     * Gets the customer phone number.
+     *
+     * @return A string representing customer phone number.
      */
     public String getCustomerPhone() {
         return customerPhone;
     }
 
     /**
-     * @param customerPhone to set
+     * Sets the customer phone number.
+     *
+     * @param customerPhone A string containing the customer phone number.
      */
     public void setCustomerPhone(String customerPhone) {
         this.customerPhone = customerPhone;
     }
 
     /**
-     *@return the customer country
+     * Gets the customer country.
+     *
+     * @return A string representing the customer country name.
      */
     public String getCustomerCountry() {
         return customerCountry;
     }
 
     /**
-     * @param customerCountry to set
+     * Sets the customer country.
+     *
+     * @param customerCountry A string containing customer country name.
      */
     public void setCustomerCountry(String customerCountry) {
         this.customerCountry = customerCountry;
     }
 
     /**
-     @return the country ID
+     * Gets the country ID.
+     *
+     * @return An integer representing the customer country.
      */
     public int getCountryId() {
         return countryId;
     }
 
     /**
-     @param countryId to set
+     * Sets the country ID.
+     *
+     * @param countryId  An integer containing the customer country.
      */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
 
     /**
-     * @return the division ID
+     * Gets the division ID.
+     *
+     * @return An integer representing the customer state/province.
      */
     public int getDivisionId() {
         return divisionId;
     }
 
     /**
-     * @param divisionId to set
+     * Sets the division ID.
+     *
+     * @param divisionId An integer containing the customer state/province.
      */
     public void setDivisionId(int divisionId) {
         this.divisionId = divisionId;
     }
 
     /**
-     * @return the division
+     * Gets the customer division name.
+     *
+     * @return A string representing the customer state/province name.
      */
     public String getDivision() {
         return division;
     }
 
     /**
-     * @param division to set
+     * Sets the customer division name.
+     *
+     * @param division A string containing the customer state/province name.
      */
     public void setDivision(String division) {
         this.division = division;
     }
 
     /**
-     * @return the customer postal code
+     * Gets the customer postal code.
+     *
+     * @return A string representing the customer postal code.
      */
     public String getCustomerPostal() {
         return customerPostal;
     }
 
     /**
-     * @param customerPostal to set
+     * Sets the customer postal code.
+     *
+     * @param customerPostal A string containing the customer postal code.
      */
     public void setCustomerPostal(String customerPostal) {
         this.customerPostal = customerPostal;
     }
 
+    /**
+     * Gets the sum of customers.
+     *
+     * @return An integer representing the sum of customers.
+     */
     public int getTotalCustomers() {
         return totalCustomers;
     }
 
+    /**
+     * Sets the sum of customers.
+     *
+     * @param totalCustomers An integer containing the sum of customers.
+     */
     public void setTotalCustomers(int totalCustomers) {
         this.totalCustomers = totalCustomers;
     }
