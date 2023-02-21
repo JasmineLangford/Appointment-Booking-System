232ee0620cdbe9ae96cee58daabfd38f4176c094
Jasmine Langford <jkhatt0788@gmail.com>
2023-02-21 09:06:25 -0600

    Final Javadoc edits

--- src/model/Logger.java
+++ src/model/Logger.java
@@ -5,16 +5,18 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * This class tracks user activity.
+ * This class tracks user activity by recording all user log-in attempts (successful and unsuccessful), date and
+ * timestamps to a text file. Each new record is appended to the last record.
  */
 public class Logger {
 
     private static final String log = "login_activity.txt";
 
     /**
-     * This method keeps record of all login attempts
+     * This method sets up the user activity tracker.
      *
      * @param username is the username used for the login attempt by the end-user
+     * @param trackerFormat is used to format date and time
      * @param success is used based on the end-user's login attempt
      */
     public static void trackerLog (String username, DateTimeFormatter trackerFormat, Boolean success){
@@ -22,11 +24,9 @@ public class Logger {
             BufferedWriter bw = new BufferedWriter(new FileWriter(log, true));
             bw.write ("Username: " + username + " " + "Date/Time Stamp: " + trackerFormat.format(ZonedDateTime.now())
                      + " " + "Attempt: " + (success ? "Successful" : "Failed") + "\n");
-            bw.flush();
             bw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
