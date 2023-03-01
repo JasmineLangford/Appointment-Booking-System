9f7874bef65060657efbfac71ce35b1e4ca9dcba
Jasmine Langford <jkhatt0788@gmail.com>
2023-03-01 01:01:16 -0500

    Updates to reflect user ID and username for combo box selections

--- src/model/User.java
+++ src/model/User.java
@@ -4,9 +4,26 @@ package model;
  * Model class for user.
  */
 public class User {
+
+    public User() {
+
+    }
+    // override format for combo box selection display
+    @Override
+    public String toString(){
+        return (getUserID() + " " + getUsername());
+    }
+
     private String username;
     private String password;
+    private int userID;
 
+    public User (int userID, String userName, String password){
+        this.userID = userID;
+        this.username = userName;
+        this.password = password;
+
+    }
     /**
      * @return the username
      */
@@ -33,5 +50,19 @@ public class User {
      */
     public void setPassword(String password){
         this.password = password;
+    }
+
+    /**
+     * @return user ID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
     }
 }
