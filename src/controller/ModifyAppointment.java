ede7f6ee7099c3cb731c1b9bdfeaeb2d15205328
Jasmine Langford <jkhatt0788@gmail.com>
2023-03-01 01:01:58 -0500

    Modify functionality completed

--- src/controller/ModifyAppointment.java
+++ src/controller/ModifyAppointment.java
@@ -1,20 +1,20 @@
 package controller;
 
-import DAO.AppointmentDAO;
-import DAO.ContactDAO;
-import DAO.CustomerDAO;
-import DAO.UserDAO;
+import DAO.*;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.Region;
 import model.Appointment;
+import model.Customer;
+
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -48,7 +48,7 @@ public class ModifyAppointment implements Initializable {
     @FXML
     private TextField descText;
     @FXML
-    private ComboBox<CustomerDAO> customerCombo;
+    private ComboBox<Customer> customerCombo;
     @FXML
     private ComboBox<UserDAO> userCombo;
 
@@ -56,7 +56,7 @@ public class ModifyAppointment implements Initializable {
     // observable lists for combo boxes
     ObservableList<ContactDAO> contacts = ContactDAO.allContacts();
     ObservableList <UserDAO> users = UserDAO.allUsers();
-    ObservableList <CustomerDAO> customers = CustomerDAO.allCustomerSelections();
+    ObservableList <Customer> customers = CustomerDAO.allCustomers();
 
     Appointment modAppointment = new Appointment();
 
@@ -88,19 +88,36 @@ public class ModifyAppointment implements Initializable {
     }
 
     /**
-     * This populates the date from the selected appointment tableview from Main Menu.
+     * This method populates the selected appointment row of data from the Main Menu.
+     *
+     * @param appointment The appointment from the appointment model.
      */
     public void sendAppointment(Appointment appointment) {
 
         try{
-        modAppointment = appointment;
+            modAppointment = appointment;
 
-        for (ContactDAO a:contactModCombo.getItems()) {
-            if(a.getContactID() == appointment.getContactID()){
-                contactModCombo.getSelectionModel().select(a);
-                break;
+            for (ContactDAO a:contactModCombo.getItems()) {
+                if(a.getContactID() == appointment.getContactID()){
+                    contactModCombo.getSelectionModel().select(a);
+                    break;
+                }
             }
-        }
+
+            for (Customer c : customerCombo.getItems()) {
+                if(c.getCustomerId() == modAppointment.getCustomerID()) {
+                    customerCombo.getSelectionModel().select(c);
+                    break;
+                }
+            }
+
+            for (UserDAO u : userCombo.getItems()) {
+                if(u.getUserID() == modAppointment.getUserID()) {
+                    userCombo.getSelectionModel().select(u);
+                    break;
+                }
+            }
+
         apptId.setText(String.valueOf(modAppointment.getAppointmentID()));
         startDatePicker.setValue(modAppointment.getStart().toLocalDate());
         startCombo.setValue(modAppointment.getStart().toLocalTime());
@@ -110,84 +127,204 @@ public class ModifyAppointment implements Initializable {
         typeText.setText(modAppointment.getType());
         titleText.setText(modAppointment.getTitle());
         descText.setText(modAppointment.getDescription());
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
     
-        /**
-         * Modified fields will be updated in the database and reflect on appointment table on main menu.
-         *
-         * @param actionEvent Save button is clicked.
-         */
-    public void onSaveModAppointment(ActionEvent actionEvent) {
-        // input validation messages
-        try {
-            if (startDatePicker.getValue() == null || endDatePicker.getValue() == null ||
-                    startCombo.getValue() == null || endCombo.getValue() == null) {
+    /**
+     * This method will save the modified appointment data.
+     *
+     * @param actionEvent The save button is clicked.
+     */
+    public void onSaveModAppointment(ActionEvent actionEvent) throws SQLException {
 
-                Alert noSelection = new Alert(Alert.AlertType.ERROR, "Please select start date/time and end " +
-                        "date/time.");
-                Optional<ButtonType> results = noSelection.showAndWait();
-                if (results.isPresent() && results.get() == ButtonType.OK)
-                    return;
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            if (contactModCombo.getSelectionModel().isEmpty() || typeText.getText().isEmpty() || titleText.getText().isEmpty() ||
-                    descText.getText().isEmpty() || locationText.getText().isEmpty()) {
-                Alert emptyField = new Alert(Alert.AlertType.ERROR, "One or more fields are empty. Please enter a" +
-                        " value in each field.");
-                Optional<ButtonType> results = emptyField.showAndWait();
-                if (results.isPresent() && results.get() == ButtonType.OK)
-                    return;
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-
+        // input validation messages: start and end date pickers
         try {
             if (endDatePicker.getValue().isBefore(startDatePicker.getValue())) {
                 Alert invalidDate = new Alert(Alert.AlertType.ERROR, "End date cannot be before start date.");
                 Optional<ButtonType> results = invalidDate.showAndWait();
                 if (results.isPresent() && results.get() == ButtonType.OK)
-                    return;
+                    invalidDate.setOnCloseRequest(Event::consume);
+                return;
             }
-        } catch (Exception e) {
-            return;
+        } catch (Exception DateException) {
+            System.out.println("Caught DateException");
         }
 
         try {
-            if (endCombo.getValue().isBefore(startCombo.getValue())) {
-                Alert invalidDate = new Alert(Alert.AlertType.ERROR, "End time cannot be before start time.");
+            if (startDatePicker.getValue().isBefore(LocalDate.now())) {
+                Alert invalidDate = new Alert(Alert.AlertType.ERROR, "Start date has already passed. Please " +
+                        "select another date.");
                 Optional<ButtonType> results = invalidDate.showAndWait();
                 if (results.isPresent() && results.get() == ButtonType.OK)
-                    return;
+                    invalidDate.setOnCloseRequest(Event::consume);
+                return;
             }
-        } catch (Exception e) {
-            return;
+        } catch (Exception DateException) {
+            System.out.println("Caught DateException");
         }
 
-        LocalDateTime modStartDateTime = LocalDateTime.of(startDatePicker.getValue(),startCombo.getValue());
-        LocalDateTime modEndDateTime = LocalDateTime.of(endDatePicker.getValue(),endCombo.getValue());
+        try {
+            if (startDatePicker.getValue().getDayOfWeek().equals(DayOfWeek.SATURDAY) ||
+                    startDatePicker.getValue().getDayOfWeek().equals(DayOfWeek.SUNDAY) ||
+                    endDatePicker.getValue().getDayOfWeek().equals(DayOfWeek.SATURDAY) ||
+                    endDatePicker.getValue().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                Alert invalidDate = new Alert(Alert.AlertType.ERROR, "Start/End dates must be a weekday " +
+                        "(Monday-Friday).");
+                Optional<ButtonType> results = invalidDate.showAndWait();
+                if (results.isPresent() && results.get() == ButtonType.OK)
+                    invalidDate.setOnCloseRequest(Event::consume);
+                return;
+            }
+        } catch (Exception DateException) {
+            System.out.println("Caught DateException");
+        }
+
+        // input validation messages: start and end time combo boxes
+        LocalDateTime localStart = LocalDateTime.of(startDatePicker.getValue(),startCombo.getValue());
+        LocalDateTime localEnd = LocalDateTime.of(endDatePicker.getValue(),endCombo.getValue());
+        try {
+            if (localEnd.isBefore(localStart)) {
+                Alert invalidTime = new Alert(Alert.AlertType.ERROR, "End date/time cannot be before start date/time.");
+                Optional<ButtonType> results = invalidTime.showAndWait();
+                if (results.isPresent() && results.get() == ButtonType.OK)
+                    invalidTime.setOnCloseRequest(Event::consume);
+                return;
+            }
+        }catch (Exception TimeException){
+            System.out.println("Caught TimeException");
+        }
+
+        try{
+            if (startCombo.getValue().equals(endCombo.getValue())) {
+                Alert invalidTime = new Alert(Alert.AlertType.ERROR, "End time cannot be the same as start " +
+                        "time. Please select another end time.");
+                invalidTime.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                Optional<ButtonType> results = invalidTime.showAndWait();
+                if (results.isPresent() && results.get() == ButtonType.OK)
+                    invalidTime.setOnCloseRequest(Event::consume);
+                return;
+            }
+        }catch (Exception TimeException){
+            System.out.println("Caught TimeException");
+        }
+
+        // input validation: empty text fields
+        try {
+            if (typeText.getText().isEmpty() || titleText.getText().isEmpty() ||
+                    descText.getText().isEmpty() || locationText.getText().isEmpty()) {
+                Alert emptyField = new Alert(Alert.AlertType.ERROR, "One or more fields are empty. Please enter a" +
+                        " value in each field.");
+                Optional<ButtonType> results = emptyField.showAndWait();
+                if (results.isPresent() && results.get() == ButtonType.OK)
+                    emptyField.setOnCloseRequest(Event::consume);
+                return;
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        int modApptID = Integer.parseInt(apptId.getText());
+        LocalDate modStartDate = startDatePicker.getValue();
+        LocalTime modStartTime = startCombo.getValue();
+        LocalDate modEndDate = endDatePicker.getValue();
+        LocalTime modEndTime = endCombo.getValue();
+        LocalDateTime startDateTime = LocalDateTime.of(startDatePicker.getValue(),startCombo.getValue());
+        LocalDateTime endDateTime = LocalDateTime.of(endDatePicker.getValue(),endCombo.getValue());
         int modContact = contactModCombo.getSelectionModel().getSelectedItem().getContactID();
         String modType = typeText.getText();
         String modTitle = titleText.getText();
         String modDescription = descText.getText();
         String modLocation = locationText.getText();
-        int modCustID = Integer.parseInt(String.valueOf(customerCombo.getValue()));
-        int modUserID = Integer.parseInt(String.valueOf(userCombo.getValue()));
+        int modCustID = customerCombo.getSelectionModel().getSelectedItem().getCustomerId();
+        int modUserID = userCombo.getSelectionModel().getSelectedItem().getUserID();
 
+        LocalDateTime dateTimeStart = LocalDateTime.of(modStartDate,modStartTime);
+        LocalDateTime dateTimeEnd = LocalDateTime.of(modEndDate,modEndTime);
+
+        ZonedDateTime userStart = ZonedDateTime.of(dateTimeStart,ZoneId.systemDefault());
+        ZonedDateTime userEnd = ZonedDateTime.of(dateTimeEnd,ZoneId.systemDefault());
+        ZonedDateTime businessStart = ZonedDateTime.of(modStartDate, LocalTime.of(8,0),ZoneId.of("US/Eastern"));
+        ZonedDateTime businessEnd = ZonedDateTime.of(modEndDate, LocalTime.of(22,0), ZoneId.of("US/Eastern"));
+
+        // checking for selected appointment dates/times are out of business hours
+        if(userStart.isBefore(businessStart) || userStart.isAfter(businessEnd) || userEnd.isBefore(businessStart)
+                || userEnd.isAfter(businessEnd)) {
+            Alert businessHourConflict = new Alert(Alert.AlertType.ERROR, "Time is outside of normal business hours (8am-10pm EST).");
+            Optional<ButtonType> results = businessHourConflict.showAndWait();
+            if (results.isPresent() && results.get() == ButtonType.OK)
+                return;
+        }
+
+        // input validation messages for conflicting appointments
+        if(modStartDate != modAppointment.getStart().toLocalDate() ||
+                modEndDate != modAppointment.getEnd().toLocalDate() ||
+                modStartTime != modAppointment.getStart().toLocalTime() ||
+                modEndTime != modAppointment.getEnd().toLocalTime())  {
+            ObservableList<Appointment> getAllAppointments = AppointmentDAO.allAppointments();
+            try {
+                for (Appointment apptConflicts : getAllAppointments) {
+
+                    if (modCustID == apptConflicts.getCustomerID() && (dateTimeStart.isEqual(apptConflicts.getStart()) &&
+                            dateTimeEnd.isEqual(apptConflicts.getEnd()))) {
+
+                        Alert apptConflict = new Alert(Alert.AlertType.ERROR, "This customer already has an existing " +
+                                "appointment for this date and time.");
+                        apptConflict.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                        Optional<ButtonType> results = apptConflict.showAndWait();
+                        if (results.isPresent() && results.get() == ButtonType.OK)
+                            return;
+                    }
+
+                    if (modCustID == apptConflicts.getCustomerID() && ((dateTimeStart.isBefore(apptConflicts.getStart()) ||
+                            dateTimeStart.isEqual(apptConflicts.getStart()))) && ((dateTimeEnd.isAfter(apptConflicts.getEnd()) ||
+                            dateTimeEnd.isEqual(apptConflicts.getEnd())))) {
+
+                        Alert apptConflict = new Alert(Alert.AlertType.ERROR, "This meeting overlaps with the " +
+                                "customer's existing appointment.");
+                        apptConflict.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                        Optional<ButtonType> results = apptConflict.showAndWait();
+                        if (results.isPresent() && results.get() == ButtonType.OK)
+                            return;
+                    }
+
+                    if (modCustID == apptConflicts.getCustomerID() && ((dateTimeStart.isAfter(apptConflicts.getStart()) &&
+                            dateTimeStart.isBefore(apptConflicts.getEnd())))) {
+
+                        Alert apptConflict = new Alert(Alert.AlertType.ERROR, "Start time overlaps with existing " +
+                                "appointment.");
+                        Optional<ButtonType> results = apptConflict.showAndWait();
+                        if (results.isPresent() && results.get() == ButtonType.OK)
+                            return;
+                    }
+
+                    if (modCustID == apptConflicts.getCustomerID() && ((dateTimeEnd.isAfter(apptConflicts.getStart()) &&
+                            ((dateTimeEnd.isBefore(apptConflicts.getEnd()) || dateTimeEnd.isEqual(apptConflicts.getEnd())))))) {
+
+                        Alert apptConflict = new Alert(Alert.AlertType.ERROR, "End time overlaps with existing " +
+                                "appointment.");
+                        Optional<ButtonType> results = apptConflict.showAndWait();
+                        if (results.isPresent() && results.get() == ButtonType.OK)
+                            return;
+                    }
+                }
+            } catch (Exception AppointmentConflicts) {
+                System.out.println("Caught AppointmentConflicts");
+            }
+        }
+        else{
+            System.out.println("No changes to dates and times.");
+        }
         try {
+            modAppointment.setAppointmentID(modApptID);
             modAppointment.setTitle(modTitle);
             modAppointment.setDescription(modDescription);
             modAppointment.setLocation(modLocation);
             modAppointment.setType(modType);
-            modAppointment.setStart(modStartDateTime);
-            modAppointment.setEnd(modEndDateTime);
+            modAppointment.setStart(startDateTime);
+            modAppointment.setEnd(endDateTime);
             modAppointment.setCustomerID(modCustID);
             modAppointment.setUserID(modUserID);
             modAppointment.setContactID(modContact);
@@ -197,8 +334,8 @@ public class ModifyAppointment implements Initializable {
             Optional<ButtonType> result = modAppointment.showAndWait();
             if (result.isPresent() && result.get() == ButtonType.YES) {
 
-                AppointmentDAO.modifyAppointment(modTitle, modDescription, modLocation, modType,
-                        modStartDateTime,modEndDateTime,modCustID, modUserID, String.valueOf(modContact));
+                AppointmentDAO.modifyAppointment(modApptID,modTitle, modDescription, modLocation, modType,
+                        startDateTime,endDateTime,modCustID, modUserID, String.valueOf(modContact));
 
                 MainMenu.toMainMenu(actionEvent);
             } else {
@@ -212,9 +349,10 @@ public class ModifyAppointment implements Initializable {
     }
 
     /**
-     * This navigates the end-user back to the Main Menu.
+     * This method navigates the end-user back to the Main Menu.
      *
-     * @param actionEvent Cancel button is clicked.
+     * @param actionEvent The cancel button is clicked.
+     * @throws IOException The exception thrown if there is an I/O error for the alert.
      */
     public void toMainMenu(ActionEvent actionEvent) throws IOException {
         Alert noChange = new Alert(Alert.AlertType.INFORMATION, "There were no changes made to this " +
