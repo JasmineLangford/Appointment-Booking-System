1f20a6c9a2d0e5ca7d8727bd08559fa3c62bc278
Jasmine Langford <jkhatt0788@gmail.com>
2023-03-01 22:28:51 -0500

    Final

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,18 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
- * This class creates the scheduler appointment.
+ * This class creates the scheduler application.
  */
 
 public class Main extends Application {
+
     /**
-     * This opens the Login screen which will allow users to access the scheduler application upon successful sign in.
-     * @param primaryStage top-level container for all scenes.
+     * This method opens the Login screen which will allow users to access the scheduler application upon successful
+     * login.
+     *
+     * @param primaryStage The top-level container for all scenes.
+     * @throws Exception The exception to throw.
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -34,14 +39,13 @@ public class Main extends Application {
     }
 
     /**
-     * This is called to run the program.
+     * This method is called to run the application.
      */
     public static void main(String[] args) {
 
+        // database connection
         JDBC.openConnection();
         launch();
         JDBC.closeConnection();
-
     }
-
 }
