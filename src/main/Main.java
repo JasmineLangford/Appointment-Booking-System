0938c69cf383a8a042dd8e69c487b2768feef1b3
Jasmine Langford <jkhatt0788@gmail.com>
2023-06-08 02:51:25 -0400

    Set screen to load with window - omitted necessary code

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,18 +9,13 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.util.Locale;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
 /**
  * This class creates the appointment booking application.
  */
 
 public class Main extends Application {
-    private double xOffset = 0;
-    private double yOffset = 0;
 
     /**
      * This method is called to run the application.
@@ -50,26 +45,15 @@ public class Main extends Application {
         // Set login scene
         AnchorPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Login.fxml")));
         Scene scene = new Scene(root, 600, 380);
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setTitle("Appointment Booking System");
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
         root.setStyle("-fx-background-color: transparent;");
         root.setStyle("-fx-background-radius: 30px 30px 30px 30px;");
 
-        // Scene is able to move on mouse drag
-        scene.setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-        root.setOnMouseDragged(event -> {
-            primaryStage.setX(event.getScreenX() - xOffset);
-            primaryStage.setY(event.getScreenY() - yOffset);
-        });
-
         primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
         primaryStage.show();
-
-        ResourceBundle rb = ResourceBundle.getBundle("Nat", Locale.getDefault());
-        primaryStage.setTitle(rb.getString("loginScreenHeader"));
     }
 }
