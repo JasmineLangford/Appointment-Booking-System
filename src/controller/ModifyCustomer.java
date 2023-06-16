dadc1de938e6b7b9f6c3652d5d1fc54ee9311294
Jasmine Langford <jkhatt0788@gmail.com>
2023-06-16 01:26:02 -0400

    Final - modified Javadocs and comments

--- src/controller/ModifyCustomer.java
+++ src/controller/ModifyCustomer.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Customer;
+
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -26,33 +27,34 @@ import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 /**
- * This class is the controller for modify-customer.fxml. The end-user is able to modify text fields and combo boxes.
- * The customer ID was auto-incremented from the database when the customer was added and is disabled. The end-user will
+ * This class is the controller for modify-customer.fxml. The user is able to modify text fields and combo boxes.
+ * The customer ID was auto-incremented from the database when the customer was added and is disabled. The user will
  * be able to save the changes to this customer by clicking the save button at the bottom of the screen or cancel
- * changes if the end-user no longer wants to modify the customer details.
+ * changes if the user no longer wants to modify the customer details.
  */
 public class ModifyCustomer implements Initializable {
-    // form fields to be pre-populated
+
+    ObservableList<CountryDAO> countries = CountryDAO.allCountries();
+    ObservableList<FirstLevelDAO> divisions = FirstLevelDAO.allFirstLevelDivision();
+    Customer.RegularCustomer modCustomer = new Customer.RegularCustomer();
+    @FXML
+    private TextField customerType;
+    @FXML
+    private TextField loyaltyPointsText;
     @FXML
     private TextField customerIdField;
     @FXML
     private TextField customerNameText;
     @FXML
-    private  TextField customerAddressText;
+    private TextField customerAddressText;
     @FXML
-    private  TextField customerPhoneText;
+    private TextField customerPhoneText;
     @FXML
-    private  TextField customerPostalText;
+    private TextField customerPostalText;
     @FXML
     private ComboBox<CountryDAO> countryModCombo;
     @FXML
     private ComboBox<FirstLevelDAO> firstLevelModCombo;
-
-    // observable lists for combo boxes
-    ObservableList<CountryDAO> countries = CountryDAO.allCountries();
-    ObservableList<FirstLevelDAO> divisions = FirstLevelDAO.allFirstLevelDivision();
-
-    Customer modCustomer = new Customer();
 
     public ModifyCustomer() throws SQLException {
     }
@@ -61,70 +63,63 @@ public class ModifyCustomer implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Modify Customer initialized.");
 
-        // country combo box
+        // Combo boxes
         countryModCombo.setItems(countries);
-
-        // state/province combo box
         firstLevelModCombo.setItems(divisions);
     }
 
     /**
-     * This method populates the selected customer row of data from Main Menu.
+     * This method populates the selected customer information to the modify customer screen.
      *
      * @param customer The customer represented from the customer model.
      * @throws SQLException The exception to throw if there are errors querying the countries or divisions for the
-     * combo boxes.
+     *                      combo boxes.
      */
-    public void sendCustomer(Customer customer) throws SQLException {
+    public void sendCustomer(Customer.RegularCustomer customer) throws SQLException {
         modCustomer = customer;
 
         for (CountryDAO c : countryModCombo.getItems()) {
-            if(c.getCountryId() == modCustomer.getCountryId()){
+            if (c.getCountryId() == modCustomer.getCountryId()) {
                 countryModCombo.getSelectionModel().select(c);
 
                 Customer modSelection = countryModCombo.getSelectionModel().getSelectedItem();
-                firstLevelModCombo.setItems(FirstLevelDAO.allFirstLevelDivision().stream()
-                        .filter(firstLevel -> firstLevel.getCountryId() == modSelection.getCountryId())
-                        .collect(Collectors.toCollection(FXCollections::observableArrayList)));
+                firstLevelModCombo.setItems(FirstLevelDAO.allFirstLevelDivision().stream().filter(firstLevel -> firstLevel.getCountryId() == modSelection.getCountryId()).collect(Collectors.toCollection(FXCollections::observableArrayList)));
 
                 firstLevelModCombo.setVisibleRowCount(5);
                 break;
             }
         }
         for (FirstLevelDAO f : firstLevelModCombo.getItems()) {
-            if(f.getDivisionId() == modCustomer.getDivisionId()) {
+            if (f.getDivisionId() == modCustomer.getDivisionId()) {
                 firstLevelModCombo.getSelectionModel().select(f);
                 break;
             }
         }
         customerIdField.setText(String.valueOf(modCustomer.getCustomerId()));
-
         customerNameText.setText(modCustomer.getCustomerName());
         customerAddressText.setText(modCustomer.getCustomerAddress());
         customerPhoneText.setText(modCustomer.getCustomerPhone().replaceAll("\\D", ""));
         customerPostalText.setText(modCustomer.getCustomerPostal());
+        customerType.setText((modCustomer.getCustomerType()));
+        loyaltyPointsText.setText(String.valueOf(modCustomer.getLoyaltyPoints()));
     }
 
     /**
-     * This method filters the divisions based on the end-user updating the country.
+     * This method filters the divisions based on the user updating the country.
      *
      * @throws SQLException The exception to throw if there are errors querying the divisions for the combo box.
      */
     public void onModCountry() throws SQLException {
-
-        // filters first-level division combo box
         Customer modSelection = countryModCombo.getSelectionModel().getSelectedItem();
 
-        firstLevelModCombo.setItems(FirstLevelDAO.allFirstLevelDivision().stream()
-                .filter(firstLevel -> firstLevel.getCountryId() == modSelection.getCountryId())
-                .collect(Collectors.toCollection(FXCollections::observableArrayList)));
+        firstLevelModCombo.setItems(FirstLevelDAO.allFirstLevelDivision().stream().filter(firstLevel -> firstLevel.getCountryId() == modSelection.getCountryId()).collect(Collectors.toCollection(FXCollections::observableArrayList)));
 
         firstLevelModCombo.getSelectionModel().selectFirst();
         firstLevelModCombo.setVisibleRowCount(5);
     }
 
     /**
-     * This method will save any modified customer data.
+     * This method will save any changes to customer information.
      *
      * @param actionEvent The save button is clicked.
      */
@@ -136,20 +131,19 @@ public class ModifyCustomer implements Initializable {
         String modPhone = customerPhoneText.getText();
         String modPostal = customerPostalText.getText();
         String modCountry = countryModCombo.getValue().toString();
+        String modType = customerType.getText();
+        int modLoyaltyPoints = Integer.parseInt(loyaltyPointsText.getText());
 
-        // input validation message: empty text field(s)
-        if (modCustomerName.isEmpty() || modAddress.isEmpty() || modPhone.isEmpty() || modCountry.isEmpty() ||
-                modPostal.isEmpty()) {
+        // Input validation messages
+        if (modCustomerName.isEmpty() || modAddress.isEmpty() || modPhone.isEmpty() || modCountry.isEmpty() || modPostal.isEmpty()) {
 
-            Alert emptyField = new Alert(Alert.AlertType.ERROR, "One or more text fields are empty. Please enter a "
-                    + "value in each field.");
+            Alert emptyField = new Alert(Alert.AlertType.ERROR, "One or more text fields are empty. Please enter a " + "value in each field.");
             emptyField.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
             Optional<ButtonType> result = emptyField.showAndWait();
-            if (result.isPresent() && result.get() == ButtonType.OK)
-                return;
+            if (result.isPresent() && result.get() == ButtonType.OK) return;
         }
 
-        try{
+        try {
             int modDivision = firstLevelModCombo.getSelectionModel().getSelectedItem().getDivisionId();
 
             modCustomer.setCustomerId(modCustomerID);
@@ -159,15 +153,15 @@ public class ModifyCustomer implements Initializable {
             modCustomer.setCustomerPhone(modPhone);
             modCustomer.setCustomerCountry(modCountry);
             modCustomer.setDivisionId(modDivision);
+            modCustomer.setLoyaltyPoints(modLoyaltyPoints);
 
-            Alert modCustomer = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to modify this " +
-                    "customer?", ButtonType.YES, ButtonType.NO);
+            Alert modCustomer = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to modify this " + "customer?", ButtonType.YES, ButtonType.NO);
             Optional<ButtonType> result = modCustomer.showAndWait();
-            if (result.isPresent() && result.get() == ButtonType.YES){
-                CustomerDAO.modifyCustomer(modCustomerID,modCustomerName,modAddress,modPostal,modPhone,modDivision);
+            if (result.isPresent() && result.get() == ButtonType.YES) {
+                CustomerDAO.modifyCustomer(modCustomerID, modCustomerName, modAddress, modPostal, modPhone, modDivision, modType, modLoyaltyPoints);
                 toCustomers(actionEvent);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -182,6 +176,12 @@ public class ModifyCustomer implements Initializable {
         toCustomers(actionEvent);
     }
 
+    /**
+     * This method takes the user to the customers screen.
+     *
+     * @param actionEvent Customers is clicked (located on the left panel).
+     * @throws IOException The exception to throw if I/O error occurs.
+     */
     public void toCustomers(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/customers.fxml"))));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
